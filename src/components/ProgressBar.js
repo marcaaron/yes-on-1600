e9bb3b471c574e9cbbf687ed1e75ea789cc08add
Den Temple <developer@dentemple.com>
2018-03-15 20:00:19 -0400

    Fix console error (missing keys)

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,11 +4,11 @@ class ProgressBar extends Component {
 	render() {
 		return (
 			<div className="progress-bar">
-				{this.props.questionArray && this.props.questionArray.map((el,index)=>{
-					if(index <= this.props.index){
-						return <div className="progress-point-active"></div>
-					}else{
-						return <div className="progress-point"></div>
+				{this.props.questionArray && this.props.questionArray.map((el, index) => {
+					if (index <= this.props.index) {
+						return <div key={index} className="progress-point-active"></div>
+					} else {
+						return <div key={index} className="progress-point"></div>
 					}
 				})}
 				{/* <div className="progress-line"></div> */}
