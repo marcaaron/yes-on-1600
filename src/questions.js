270cc165cd5fd068168c7de3e123215cd1f36898
HarryAKA <harryaka@gmail.com>
2018-03-28 23:02:06 -0400

    edit 'Size of Household' tip (#32)

--- src/questions.js
+++ src/questions.js
@@ -11,7 +11,7 @@ const questions = {
 				val: 20,
 				error:'Please enter a household size between 1-20.'
 			},
-			tip:'The size of your household includes your spouse, if you have one, and any individuals that qualify as dependents.',
+			tip:'The size of your household includes yourself, your spouse (if you have one), and any individuals that qualify as dependents.',
 			indexToHide:[3],
 			condition:1
 		},
