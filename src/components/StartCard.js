eeebb74e40a618aa0acd39ac92f5287a7601bccf
Den Temple <developer@dentemple.com>
2018-04-07 15:33:37 -0400

    Swapped Individual & Business profiles

--- src/components/StartCard.js
+++ src/components/StartCard.js
@@ -7,20 +7,20 @@ class StartCard extends Component {
 			  <button
 				  onClick={this.props.handleUserType}
 				  type="submit"
-				  value="business">
-				  I'm a Business Owner
-			  </button>
-			  <p className="button-sub-text">
-				  <strong>Business Owners:</strong><em> grab your HR person or accountant.</em></p>
-			  <button
-				  onClick={this.props.handleUserType}
-				  type="submit"
 				  value="individual">
 				  I'm an Individual
 			  </button>
 			  <p className="button-sub-text">
 				  <strong>Individuals:</strong><em> To get the most accurate results, grab last year's Tax Return, a paystub that includes insurance premium deduction and an estimate of your out-of-pocket healthcare costs.</em></p>
-		  </div>
+					<button
+				  onClick={this.props.handleUserType}
+				  type="submit"
+				  value="business">
+				  I'm a Business Owner
+			  </button>
+			  <p className="button-sub-text">
+				  <strong>Business Owners:</strong><em> grab your HR person or accountant.</em></p>
+			</div>
 	  );
 	}
 };
